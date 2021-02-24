bcc7f51527b4a8b5c8b32d34589fff93a121e884
Oona Laitamaki <oona.laitamaki@tuni.fi>
2021-02-24 13:02:38 +0200

    Added test cases for fragment addition and getDate function

--- app/src/androidTest/java/openfoodfacts/github/scrachx/openfood/fragments/ChangesHistoryProductTest.kt
+++ app/src/androidTest/java/openfoodfacts/github/scrachx/openfood/fragments/ChangesHistoryProductTest.kt
@@ -2,17 +2,29 @@ package openfoodfacts.github.scrachx.openfood.fragments
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.lifecycle.Lifecycle
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import junit.framework.Assert.assertTrue
 import openfoodfacts.github.scrachx.openfood.R
 import openfoodfacts.github.scrachx.openfood.features.product.view.changes_history.ChangesHistoryProductFragment
-import org.junit.Ignore
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-class ChangesProductTest {
+class ChangesHistoryProductTest {
     @Test
-    fun testChangesHistoryProductFragment() {
+    fun testFragmentAddition() {
         val scenario = launchFragmentInContainer<ChangesHistoryProductFragment>(null, R.style.OFFTheme, Lifecycle.State.CREATED)
-        Thread.sleep(5000)
+        scenario.onFragment { fragment ->
+            assertTrue(fragment.isAdded)
+        }
+    }
+
+    @Test
+    fun testGetDate() {
+        val unixTimestamp = "1614163464"
+        val scenario = launchFragmentInContainer<ChangesHistoryProductFragment>(null, R.style.OFFTheme, Lifecycle.State.CREATED)
+        scenario.onFragment { fragment ->
+            val dateTimestamp = fragment.getDate(unixTimestamp)
+            assertTrue(dateTimestamp == "February 24, 2021")
+        }
     }
 }
